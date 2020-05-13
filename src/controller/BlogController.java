966f9687e231a1f36280165fb3e7f81747d03ac6
yashasree <yashasreeaditya@gmail.com>
2020-05-13 16:26:26 +0530

    Yash049

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -1,5 +1,6 @@
 package controller;
-
+import model.Blog;
+import model.User;
 import java.io.IOException;
 import java.time.LocalDate;
 
@@ -32,19 +33,29 @@ public class BlogController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
+		String blogDetails = request.getParameter("selectedAnswers");
+		String blogData[] = blogDetails.split(",");
+		String title = blogData[0];
+		String description = blogData[1];
+		LocalDate date = LocalDate.now(); 
 		
+		Blog blog = new Blog(title, description, date);
+		blog.setTitle(title);
+		blog.setDescription(description);
+		blog.setPostedOn(date);
 		
-		
-
-		
+		System.out.println("Blog Title: "+title);
+        System.out.println(" Blog Description: "+description);
+        System.out.println("Posted on: "+date); 
+         
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
-			request.setAttribute("user",user);
+			User user = null;
+			request.setAttribute("user", user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+		
 	}
 
 }
